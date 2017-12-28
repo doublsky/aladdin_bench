48a44902b85c0ee2992f29c08f7e9c94761ecf6c
Tony Tan <doublskyp@gmail.com>
2017-12-28 12:28:17 +0800

    minor bug fix

--- md/dse.py
+++ md/dse.py
@@ -67,7 +67,7 @@ for num_atoms in [16]:
             logging.debug(dse_df)
 
 # calculate energy per bit
-dse_df.apply(pd.to_numeric, errors="ignore")
+dse_df = dse_df.apply(pd.to_numeric, errors="ignore")
 logging.debug(dse_df.dtypes)
 total_energy = dse_df["Avg Power (mW)"] * dse_df["Cycle (cycles)"] * dse_df["Cycle Time (ns)"]
 total_input_bits = dse_df["Num of Atoms"] * 3 * 32 + dse_df["Num of Atoms"] * dse_df["Num of Atoms"] * 32
