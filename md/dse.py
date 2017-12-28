7aef2c693f468ce91a8f43e42965a4d2860788e9
Tony Tan <doublskyp@gmail.com>
2017-12-28 21:05:18 +0800

    more input size

--- md/dse.py
+++ md/dse.py
@@ -21,7 +21,7 @@ def calc_energy(df):
 
 if __name__ == "__main__":
     dse_df = pd.DataFrame()
-    for num_atoms in [16]:
+    for num_atoms in [16, 32, 64]:
         for num_simd_lanes in range(1, 33):
             for cycle_time in range(1, 7):
                 # clean
