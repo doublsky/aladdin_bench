ff759b7d2407bd37276c0bfe4ef3d8fb19043d78
Tony Tan <doublskyp@gmail.com>
2017-12-28 17:39:11 +0800

    debug cpp flags

--- md/dse.py
+++ md/dse.py
@@ -5,73 +5,70 @@ Sweep md
 import subprocess as sp
 import os
 import pandas as pd
-import logging
 
-logging.basicConfig(level=logging.DEBUG)
-
-summary_filename = "md_summary"
 dse_filename = "md_dse.csv"
+summary_filename = "md_summary"
 
-dse_df = pd.DataFrame()
-for num_atoms in [16, 32, 64]:
-    for num_simd_lanes in range(1, 5):
-        for cycle_time in range(1, 7):
-            # compile with different num_atoms
-            make_cmd = [
-                "make",
-                'CPPFLAGS="-DnAtoms={} -DmaxNeighbors={}"'.format(num_atoms, num_atoms),
-                "run-trace"
-            ]
-            sp.check_call(make_cmd)
 
-            # get path to aladdin
-            aladdin_home = os.environ["ALADDION_HOME"]
-            if not aladdin_home:
-                raise Exception("ALADDION_HOME not defined")
+def calc_energy(df):
+    num_df = df.apply(pd.to_numeric, errors="ignore")
+    total_energy = num_df["Avg Power (mW)"] * num_df["Cycle (cycles)"] * num_df["Cycle Time (ns)"]
+    total_input_bits = num_df["Num of Atoms"] * 3 * 32 + num_df["Num of Atoms"] * num_df["Num of Atoms"] * 32
+    total_output_bits = num_df["Num of Atoms"] * 3 * 32
+    num_df["Energy per Input (pJ/bit)"] = total_energy / total_input_bits
+    num_df["Energy per Output (pJ/bit)"] = total_energy / total_output_bits
+    num_df.to_csv(dse_filename, index=False)
 
-            # create config file
-            config_content = "partition,cyclic,d_force_x,256,4,1\n"
-            config_content += "partition,cyclic,d_force_y,256,4,1\n"
-            config_content += "partition,cyclic,d_force_z,256,4,1\n"
-            config_content += "partition,cyclic,position_x,256,4,{}\n".format(num_simd_lanes)
-            config_content += "partition,cyclic,position_y,256,4,{}\n".format(num_simd_lanes)
-            config_content += "partition,cyclic,position_z,256,4,{}\n".format(num_simd_lanes)
-            config_content += "partition,cyclic,NL,16384,4,{}\n".format(num_simd_lanes)
-            config_content += "unrolling,md,loop_j,{}\n".format(num_simd_lanes)
-            config_content += "pipelining,1\n"
-            config_content += "cycle_time,{}\n".format(cycle_time)
+if __name__ == "__main__":
+    dse_df = pd.DataFrame()
+    for num_atoms in [16, 32]:
+        for num_simd_lanes in range(1, 2):
+            for cycle_time in range(1, 2):
+                # compile with different num_atoms
+                make_cmd = [
+                    "make",
+                    'CPPFLAGS="-DnAtoms={} -DmaxNeighbors={}"'.format(num_atoms, num_atoms),
+                    "run-trace"
+                ]
+                sp.check_call(make_cmd)
 
-            with open("config", "w") as f:
-                f.write(config_content)
+                # get path to aladdin
+                aladdin_home = os.environ["ALADDION_HOME"]
+                if not aladdin_home:
+                    raise Exception("ALADDION_HOME not defined")
 
-            aladdin_bin = os.path.join(aladdin_home, "common/aladdin")
-            aladdin_cmd = [aladdin_bin, "md", "dynamic_trace.gz", "config"]
-            sp.check_call(aladdin_cmd)
+                # create config file
+                config_content = "partition,cyclic,d_force_x,256,4,1\n"
+                config_content += "partition,cyclic,d_force_y,256,4,1\n"
+                config_content += "partition,cyclic,d_force_z,256,4,1\n"
+                config_content += "partition,cyclic,position_x,256,4,{}\n".format(num_simd_lanes)
+                config_content += "partition,cyclic,position_y,256,4,{}\n".format(num_simd_lanes)
+                config_content += "partition,cyclic,position_z,256,4,{}\n".format(num_simd_lanes)
+                config_content += "partition,cyclic,NL,16384,4,{}\n".format(num_simd_lanes)
+                config_content += "unrolling,md,loop_j,{}\n".format(num_simd_lanes)
+                config_content += "pipelining,1\n"
+                config_content += "cycle_time,{}\n".format(cycle_time)
 
-            # process summary
-            summary = pd.read_table(
-                summary_filename,
-                header=None,
-                sep=":\s*",
-                skiprows=3,
-                skipfooter=3,
-                engine="python",
-                index_col=0
-            )
-            summary = summary.transpose()
-            summary["Num of Atoms"] = num_atoms
-            summary["Num of SIMD Lanes"] = num_simd_lanes
-            summary["Cycle Time (ns)"] = cycle_time
-            logging.debug(summary)
-            dse_df = dse_df.append(summary)
-            logging.debug(dse_df)
+                with open("config", "w") as f:
+                    f.write(config_content)
 
-# calculate energy per bit
-dse_df = dse_df.apply(pd.to_numeric, errors="ignore")
-logging.debug(dse_df.dtypes)
-total_energy = dse_df["Avg Power (mW)"] * dse_df["Cycle (cycles)"] * dse_df["Cycle Time (ns)"]
-total_input_bits = dse_df["Num of Atoms"] * 3 * 32 + dse_df["Num of Atoms"] * dse_df["Num of Atoms"] * 32
-total_output_bits = dse_df["Num of Atoms"] * 3 * 32
-dse_df["Energy per Input (pJ/bit)"] = total_energy / total_input_bits
-dse_df["Energy per Output (pJ/bit)"] = total_energy / total_output_bits
-dse_df.to_csv(dse_filename, index=False)
+                aladdin_bin = os.path.join(aladdin_home, "common/aladdin")
+                aladdin_cmd = [aladdin_bin, "md", "dynamic_trace.gz", "config"]
+                sp.check_call(aladdin_cmd)
+
+                # process summary
+                summary = pd.read_table(
+                    summary_filename,
+                    header=None,
+                    sep=":\s*",
+                    skiprows=3,
+                    skipfooter=3,
+                    engine="python",
+                    index_col=0
+                )
+                summary = summary.transpose()
+                summary["Num of Atoms"] = num_atoms
+                summary["Num of SIMD Lanes"] = num_simd_lanes
+                summary["Cycle Time (ns)"] = cycle_time
+                dse_df = dse_df.append(summary)
+                calc_energy(dse_df)
