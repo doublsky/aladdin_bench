d5d3a76b9be6b538c3400b645092392806d431aa
Tony Tan <doublskyp@gmail.com>
2018-01-07 21:24:04 +0800

    tune sort

--- sort/dse.py
+++ sort/dse.py
@@ -42,8 +42,8 @@ if __name__ == "__main__":
 
                 # create config file
                 ## array partition
-                config_content = "partition,cyclic,a,{},4,{}\n".format(N * 4, num_simd_lanes * 4)
-                config_content += "partition,cyclic,b,{},4,{}\n".format(N * 4, num_simd_lanes * 4)
+                config_content = "partition,cyclic,a,{},4,{}\n".format(N * 4, num_simd_lanes * 8)
+                config_content += "partition,cyclic,b,{},4,{}\n".format(N * 4, num_simd_lanes * 8)
                 config_content += "partition,cyclic,bucket,{},4,{}\n".format(N * 4 + 1, num_simd_lanes * 16)
                 config_content += "partition,cyclic,sum,{},4,{}\n".format(N // 4, num_simd_lanes)
 
