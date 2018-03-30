bf82455af24066e9f45183f2ed9348f0378c0102
Tony Tan <doublskyp@gmail.com>
2018-03-29 21:29:01 -0500

    update dse for kmp

--- kmp/dse.py
+++ kmp/dse.py
@@ -1,5 +1,5 @@
 """
-Sweep md
+Sweep kmp
 """
 
 import subprocess as sp
@@ -7,24 +7,24 @@ import os
 import pandas as pd
 import numpy as np
 
-dse_filename = "fft_dse.csv"
-summary_filename = "fft_summary"
+dse_filename = "kmp_dse.csv"
+summary_filename = "kmp_summary"
 
 
 def calc_energy(df):
     num_df = df.apply(pd.to_numeric, errors="ignore")
     total_energy = num_df["Avg Power (mW)"] * num_df["Cycle (cycles)"] * num_df["Cycle Time (ns)"]
-    total_input_bits = num_df["FFT_SIZE"] * 32 * 2
-    total_output_bits = num_df["FFT_SIZE"] * 32 * 2
+    total_input_bits = num_df["STRING_SIZE"] * 32 * 2
+    total_output_bits = num_df["STRING_SIZE"] * 32 * 2
     num_df["Energy per Input (pJ/bit)"] = total_energy / total_input_bits
     num_df["Energy per Output (pJ/bit)"] = total_energy / total_output_bits
     num_df.to_csv(dse_filename, index=False)
 
 if __name__ == "__main__":
     dse_df = pd.DataFrame()
-    for fft_size in np.power(2, range(12, 13)):
-        for num_simd_lanes in range(1, min(17, fft_size+1)):
-            for cycle_time in range(1, 7):
+    for string_size in [2048, 4096]:
+        for num_simd_lanes in [2**i for i in range(2)]:
+            for cycle_time in range(1, 2) + [10]:
                 # clean
                 sp.check_call(["make", "clean-trace"])
                 sp.check_call(["make", "clean"])
@@ -32,7 +32,7 @@ if __name__ == "__main__":
                 # compile
                 make_cmd = [
                     "make",
-                    "MACROS=-DFFT_SIZE={}".format(fft_size),
+                    "MACROS=-DSTRING_SIZE={}".format(string_size),
                     "generate"
                 ]
                 sp.check_call(make_cmd)
@@ -40,7 +40,7 @@ if __name__ == "__main__":
                 # compile
                 make_cmd = [
                     "make",
-                    "MACROS=-DFFT_SIZE={}".format(fft_size),
+                    "MACROS=-DSTRING_SIZE={}".format(string_size),
                     "run-trace"
                 ]
                 sp.check_call(make_cmd)
@@ -51,12 +51,11 @@ if __name__ == "__main__":
                     raise Exception("ALADDIN_HOME not defined")
 
                 # create config file
-                config_content = "partition,cyclic,real,{},4,{}\n".format(fft_size * 4, num_simd_lanes * 4)
-                config_content += "partition,cyclic,img,{},4,{}\n".format(fft_size * 4, num_simd_lanes * 4)
-                config_content += "partition,cyclic,real_twid,{},4,{}\n".format(fft_size * 2, num_simd_lanes)
-                config_content += "partition,cyclic,img_twid,{},4,{}\n".format(fft_size * 2, num_simd_lanes)
-                config_content += "unrolling,fft,inner,{}\n".format(num_simd_lanes)
-
+                config_content = "partition,complete,pattern,4\n"
+                config_content += "partition,cyclic,imput,{},1,{}\n".format(string_size, num_simd_lanes)
+                config_content += "partition,complete,kmpNext,16\n"
+                config_content += "partition,complete,n_matches,4\n"
+                config_content += "unrolling,kmp,k1,{}\n".format(num_simd_lanes)
                 config_content += "pipelining,1\n"
                 config_content += "cycle_time,{}\n".format(cycle_time)
 
@@ -64,7 +63,7 @@ if __name__ == "__main__":
                     f.write(config_content)
 
                 aladdin_bin = os.path.join(aladdin_home, "common/aladdin")
-                aladdin_cmd = [aladdin_bin, "fft", "dynamic_trace.gz", "config"]
+                aladdin_cmd = [aladdin_bin, "kmp", "dynamic_trace.gz", "config"]
                 sp.check_call(aladdin_cmd)
 
                 # process summary
@@ -78,7 +77,7 @@ if __name__ == "__main__":
                     index_col=0
                 )
                 summary = summary.transpose()
-                summary["FFT_SIZE"] = fft_size
+                summary["STRING_SIZE"] = string_size
                 summary["Num of SIMD Lanes"] = num_simd_lanes
                 summary["Cycle Time (ns)"] = cycle_time
                 dse_df = dse_df.append(summary)
--- kmp/kmp.h
+++ kmp/kmp.h
@@ -8,7 +8,7 @@ Implementation based on http://www-igm.univ-mlv.fr/~lecroq/string/node8.html
 #include "support.h"
 
 #define PATTERN_SIZE 4
-#define STRING_SIZE (32411)
+//#define STRING_SIZE (32411)
 
 int kmp(char pattern[PATTERN_SIZE], char input[STRING_SIZE], int32_t kmpNext[PATTERN_SIZE], int32_t n_matches[1]);
 ////////////////////////////////////////////////////////////////////////////////
